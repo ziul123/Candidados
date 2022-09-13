aaf6baafcf1f5cb354810445d4fa328c55e39c27
Victor Hugo França Lisboa <victorhugof.lisboa@gmail.com>
2022-09-13 11:15:00 -0300

    DAO do Politico

--- DAOs/politicoDAO.py
+++ DAOs/politicoDAO.py
@@ -1,9 +1,9 @@
 class Politico():
-    def __init__(self, cpf, nome, candidatura, DataNasc, foto):
+    def __init__(self, cpf, nome, candidatura, dataNasc, foto):
         self.cpf = cpf
         self.nome = nome
         self.candidatura = candidatura
-        self.DataNasc = DataNasc
+        self.dataNasc = dataNasc
         self.foto = foto
 
 class PoliticoDao():
@@ -13,8 +13,8 @@ class PoliticoDao():
     def create(self, cursor, politico):
         try:
             data = {'CPF': politico.cpf, 'nome': politico.nome,
-            'candidatura': politico.candidatura, 'DataNasc': politico.DataNasc, 'foto': politico.foto}
-            sql = "INSERT INTO Politico VALUES(%(CPF)s, %(nome)s, %(candidatura)s, %(DataNasc)s, %(foto)s)"
+            'candidatura': politico.candidatura, 'dataNasc': politico.dataNasc, 'foto': politico.foto}
+            sql = "INSERT INTO Politico VALUES(%(CPF)s, %(nome)s, %(candidatura)s, %(dataNasc)s, %(foto)s)"
             cursor.execute(sql, data)
         except Exception as ex:
             print(ex)
@@ -22,40 +22,39 @@ class PoliticoDao():
     def update(self, cursor, politico, codigo):
         try:
             data = {'CPF': politico.cpf, 'nome': politico.nome,
-            'candidatura': politico.candidatura, 'DataNasc': politico.DataNasc, 'foto': politico.foto}
-            sql = "UPDATE Politico SET Nome = %(nome)s, Candidatura = %(candidatura)s, DataNasc = %(DataNasc)s, Foto =' %(foto)s \
-                WHERE codigo = %(codigo)s"
+            'candidatura': politico.candidatura, 'dataNasc': politico.dataNasc, 'foto': politico.foto}
+            sql = "UPDATE Politico SET nome = %(nome)s, candidatura = %(candidatura)s, dataNasc = %(dataNasc)s, Foto =' %(foto)s \
+                WHERE CPF = %(CPF)s"
             cursor.execute(sql, data)
         except Exception as ex:
             print(ex)
 
-    def delete(self, cursor, codigo):
+    def delete(self, cursor, CPF):
         try:
-            sql = f"DELETE FROM endereco WHERE codigo = '{codigo}'"
+            sql = f"DELETE FROM poltico WHERE CPF = '{CPF}'"
             cursor.execute(sql)
         except Exception as ex:
             print(ex)
 
-    def find_by_id(self, cursor, codigo):
+    def findByCPF(self, cursor, CPF):
         try:
-            sql = f"SELECT * FROM endereco WHERE codigo = '{codigo}'"
+            sql = f"SELECT * FROM politico WHERE CPF = '{CPF}'"
             cursor.execute(sql)
             result = cursor.fetchone()
 
-            codigo, CEP, rua, bairro, cidade, estado, numero, complemento = result
-            endereco = Endereco(codigo, CEP, rua, bairro, cidade, estado, numero, complemento)
-            return endereco
+            CPF, nome, candidatura, dataNasc, foto = result
+            politico = Politico(CPF, nome, candidatura, dataNasc, foto)
+            return politico
         except Exception as ex:
             print(ex)
             return None
 
-    def find_all(self, cursor):
+    def findAll(self, cursor):
         try:
-            sql = "SELECT * FROM endereco"
+            sql = "SELECT * FROM politico"
             cursor.execute(sql)
             result = cursor.fetchall()
 
-            # depois ver como retornar
             return result
         except Exception as ex:
             print(ex)
