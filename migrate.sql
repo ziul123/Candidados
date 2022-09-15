72166cb2c6a282e81364daa0aa1a52844b731140
victorlisboa <victorhugof.lisboa@gmail.com>
2022-09-15 11:03:47 -0300

    Populando banco

--- migrate.sql
+++ migrate.sql
@@ -8,3 +8,8 @@ INSERT INTO politico VALUES("24309074022", "Hugo", NULL, "1997-11-19");
 INSERT INTO politico VALUES("35306870066", "Lucas", "Deputado Distrital", "2003-10-31");
 INSERT INTO politico VALUES("07429018076", "Henrique", "Deputado Estadual", "1995-02-15");
 INSERT INTO politico VALUES("43973086087", "Eduardo", "Deputado Federal", "1976-04-12");
+INSERT INTO politico VALUES("98030829060", "Thiago", "Vereador", "2000-04-01");
+INSERT INTO politico VALUES("96826773007", "Luiza", "Vereadora", "1998-05-29");
+INSERT INTO politico VALUES("18621276038", "Maria", "Deputada Estadual", "1988-07-06", NULL);
+INSERT INTO politico VALUES("83063484008", "Eduarda", NULL, "2001-06-06", NULL);
+ 
