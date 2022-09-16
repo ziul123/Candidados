4ac3d539e0514081b9f95a5ceddc83d1b29a045e
Victor Hugo França Lisboa <victorhugof.lisboa@gmail.com>
2022-09-16 20:42:04 -0300

    Minor fix

--- comandos_algebra_relacional.sql
+++ comandos_algebra_relacional.sql
@@ -1,10 +1,10 @@
 -- Esses são os comandos para representar na álgebra relacional
 
 -- Seleciona os politicos, o nome do cargo que trabalha e o nome do Orgao em que trabalha
-SELECT politico.nome AS Nome, nomeCargo AS Cargo, orgao.nome AS Orgao FROM politico, exerceCargoEm, orgao WHERE CPF = politicoCPF AND codOrg = orgaoCodOrg;
+SELECT politico.nome AS Nome, nomeCargo, orgao.nome AS Orgao FROM politico, exerceCargoEm, orgao WHERE CPF = politicoCPF AND codOrg = orgaoCodOrg;
 
 -- Seleciona Nome, Âmbito, Município e Estado de cada politico
-SELECT politico.nome AS Nome, ambito, municipio, estado FROM politico, exerceCargoEm, orgao, candidados.local
+SELECT politico.nome, ambito, municipio, estado FROM politico, exerceCargoEm, orgao, candidados.local
 WHERE CPF = politicoCPF AND codOrg = orgaoCodOrg AND codLoc = localCodLoc;
 
 -- Seleciona nome, descricao do projeto de lei e voto do politico nesse projeto
